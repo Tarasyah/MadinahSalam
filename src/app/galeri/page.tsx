02f7ebaed00d6ca55949057c11bb0adcaf7d88a3
aryamzidane@gmail.com <aryamzidane@gmail.com>
2026-06-23 22:16:38 +0700

    perbaikan eror ke 2

--- src/app/galeri/page.tsx
+++ src/app/galeri/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { InfiniteSlider } from '@/components/ui/infinite-slider';
 
